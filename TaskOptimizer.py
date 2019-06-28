c4645d3a9f2dcea4b2439b7d45d2664fbd9662da
LeszekStencel <leszek.stencel@nexio.pl>
2019-06-28 15:05:24 +0200

    change assignment tasks

--- TaskOptimizer.py
+++ TaskOptimizer.py
@@ -1,11 +1,12 @@
 import pypyodbc as pyodbc
 import operator
-from random import randint
 from datetime import datetime, timedelta
 from flask import Flask
 import json
+from uuid import uuid4
 
 app = Flask(__name__)
+
 
 def connect_database():
     try:
@@ -23,6 +24,11 @@ def connect_database():
 
 CRITICAL_PRIORITY = [8, 9, 10]
 PAUSE_TIME = 300
+START_DAY = 28800
+
+
+def get_current_time():
+    return datetime.now() - timedelta(hours=7, minutes=30)
 
 
 def load_data():
@@ -48,38 +54,19 @@ def assign_task_to_users(users, tasks, assigned_tasks_tab):
     for task in tasks:
         task_id = task[0]
         task_time = task[3]
-        if task[2] in CRITICAL_PRIORITY and assigned_tasks_tab:
-            avaliables_users = check_availability(users_id, assigned_tasks, task_time)
-            if avaliables_users:
-                user, time_to_insert = find_closes_free_user(assigned_tasks, avaliables_users, is_critic=1)
-                sql = "insert into [tasks].[UserTask] values({},{},\'{}\',\'{}\')".format(user, task_id,
-                                                                                          time_to_insert + timedelta(
-                                                                                              seconds=PAUSE_TIME),
-                                                                                          time_to_insert + timedelta(
-                                                                                              seconds=task_time + PAUSE_TIME))
-                insert_critical_task(sql)
-                tasks_to_update = [task for task in assigned_tasks if
-                                   task[1] == user and task[0] != task_id and task[3] >= time_to_insert]
-                for assigned_task in assigned_tasks:
-                    if assigned_task[2] in tasks_to_update:
-                        assigned_task[3] += timedelta(seconds=task_time + PAUSE_TIME)
-                        assigned_tasks[4] += timedelta(seconds=task_time + PAUSE_TIME)
-                if tasks_to_update:
-                    update_time_existed_user_tasks(tasks_to_update, task_time + PAUSE_TIME)
-                    assigned_tasks.append(
-                        [task[0], user, task_id, time_to_insert, time_to_insert + timedelta(seconds=task_time + PAUSE_TIME)])
-        else:
-            avaliables_users = check_availability(users_id, assigned_tasks, task_time)
-            if avaliables_users:
-                user, time_to_insert = find_closes_free_user(assigned_tasks, avaliables_users, is_critic=0)
-                sql = "insert into [tasks].[UserTask] values({},{},\'{}\',\'{}\')".format(user, task_id,
-                                                                                          time_to_insert + timedelta(
-                                                                                              seconds=PAUSE_TIME),
-                                                                                          time_to_insert + timedelta(
-                                                                                              seconds=task_time + PAUSE_TIME))
-                insert_critical_task(sql)
-                assigned_tasks.append(
-                    [0, user, task_id, time_to_insert, time_to_insert + timedelta(seconds=task_time + PAUSE_TIME)])
+
+        avaliables_users = check_availability(users_id, assigned_tasks, task_time)
+        if avaliables_users:
+            user, time_to_insert = find_closes_free_user(assigned_tasks, avaliables_users, task_time)
+            sql = "insert into [tasks].[UserTask] values({},{},\'{}\',\'{}\')".format(user, task_id,
+                                                                                      time_to_insert + timedelta(
+                                                                                          seconds=PAUSE_TIME),
+                                                                                      time_to_insert + timedelta(
+                                                                                          seconds=task_time + PAUSE_TIME))
+            insert_critical_task(sql)
+            assigned_tasks.append(
+                [uuid4(), user, task_id, time_to_insert,
+                 time_to_insert + timedelta(seconds=task_time + PAUSE_TIME)])
 
     return assigned_tasks_to_insert
 
@@ -92,29 +79,20 @@ def update_users_time(assigned_tasks, users_time):
     return users_time
 
 
-def find_closes_free_user(assigned_tasks, users, is_critic=0):
+def find_closes_free_user(assigned_tasks, users, task_time):
     assigned_users = [task[1] for task in assigned_tasks]
-    if is_critic:
-        tasks = [[task[1], task[4], task[2]] for task in assigned_tasks if
-                 task[4] > datetime.now() and task[3] < datetime.now() and task[1] in users]
-    else:
-        tasks = [[task[1], task[4], task[2]] for task in assigned_tasks if
-                 task[4] > datetime.now() and task[1] in users]
     if assigned_tasks:
         not_used_users = [user for user in users if user not in assigned_users]
+        next_tasks = find_next_task(users, assigned_tasks, task_time)
         if not_used_users:
-            return not_used_users[0], datetime.now().replace(second=0, microsecond=0)
-        if is_critic:
-            if tasks:
-                first_free_user = (min(tasks, key=lambda t: t[1]))
-            else:
-                return users[0], datetime.now().replace(second=0, microsecond=0)
+            return not_used_users[0], get_current_time().replace(second=0, microsecond=0)
         else:
             last_tasks = find_last_user_task(users, assigned_tasks)
-            first_free_user = (min(last_tasks, key=lambda t: t[1]))
+            next_and_last_tasks = last_tasks + next_tasks
+            first_free_user = (min(next_and_last_tasks, key=lambda t: t[1]))
         return first_free_user[0], first_free_user[1]
     else:
-        return users[0], datetime.now().replace(second=0, microsecond=0)
+        return users[0], get_current_time().replace(hour=7, minute=55, second=0, microsecond=0)
 
 
 def find_last_user_task(users, assigned_tasks):
@@ -124,18 +102,54 @@ def find_last_user_task(users, assigned_tasks):
         if user_tasks:
             last_tasks.append((max(user_tasks, key=lambda t: t[1])))
         else:
-            last_tasks.append([user, datetime.now().replace(second=0, microsecond=0)])
+            last_tasks.append([user, get_current_time().replace(second=0, microsecond=0)])
     return last_tasks
+
+
+def find_next_task(users, assigned_tasks, task_time):
+    next_task = []
+    for user in users:
+        all_user_tasks = [task for task in assigned_tasks if task[1] == user]
+        all_start_dates = [task[3] for task in all_user_tasks]
+        all_end_dates = [task[4] for task in all_user_tasks]
+        all_start_dates.sort()
+        all_end_dates.sort()
+        all_task_dates = list(zip(all_start_dates, all_end_dates))
+        if all_task_dates:
+            for iterator, task in enumerate(all_user_tasks):
+                if iterator == 0:
+                    if (get_current_time() - datetime.now().replace(second=0, microsecond=0, hour=0,
+                                                                    minute=0)).total_seconds() <= START_DAY and \
+                            all_task_dates[iterator][0] > get_current_time().replace(second=0, microsecond=0, hour=8,
+                                                                                     minute=0) +\
+                                                                                     timedelta(seconds=task_time + PAUSE_TIME):
+                        next_task.append([user, get_current_time().replace(second=0, microsecond=0, hour=7, minute=55)])
+
+                    elif all_task_dates[iterator][0] > get_current_time() + timedelta(seconds=task_time + PAUSE_TIME) and \
+                            (get_current_time() - datetime.now().replace(second=0, microsecond=0, hour=0,
+                                                                         minute=0)).total_seconds() >= START_DAY:
+                        next_task.append([user, get_current_time().replace(second=0, microsecond=0)])
+
+                if len(all_task_dates) > iterator > 0:
+                    if all_task_dates[iterator - 1][1] + timedelta(seconds=task_time + PAUSE_TIME) <= \
+                            all_task_dates[iterator][0]:
+                        next_task.append([user, all_task_dates[iterator - 1][1].replace(second=0, microsecond=0)])
+        else:
+            next_task.append([user, get_current_time()])
+    return next_task
 
 
 def check_availability(users, assigned_tasks, task_time):
     last_tasks = find_last_user_task(users, assigned_tasks)
+    next_tasks = find_next_task(users, assigned_tasks, task_time)
     avaliable_users = []
-    actual_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    actual_date = get_current_time().replace(hour=0, minute=0, second=0, microsecond=0)
     for user in users:
         is_avaliable = [task[0] for task in last_tasks if task[0] == user
-                           and (task[1] - actual_date).total_seconds()+ task_time + PAUSE_TIME <= 64800]
-        if is_avaliable:
+                        and (task[1] - actual_date).total_seconds() + task_time + PAUSE_TIME <= 64800]
+
+        is_avaliable2 = [task[0] for task in next_tasks if task[0] == user]
+        if is_avaliable or is_avaliable2:
             avaliable_users.append(is_avaliable[0])
     return avaliable_users
 
@@ -156,9 +170,9 @@ def insert_assigned_task_to_database(assigned_tasks):
         cursor.execute(insert)
         cursor.commit()
 
-
     cursor.close()
     conn.close()
+
 
 def insert_critical_task(sql_command):
     conn = connect_database()
@@ -167,6 +181,7 @@ def insert_critical_task(sql_command):
     cursor.commit()
     cursor.close()
     conn.close()
+
 
 def update_time_existed_user_tasks(tasks, added_time):
     conn = connect_database()
@@ -192,9 +207,7 @@ def optymize():
     sorted_by_prioryty_tasks = tasks[:]
     sorted_by_prioryty_tasks.sort(key=operator.itemgetter(2), reverse=True)
     new_assigned = assign_task_to_users(users, sorted_by_prioryty_tasks, assigned_tasks)
-    insert_assigned_task_to_database(new_assigned)
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-
 
 
 if __name__ == '__main__':
